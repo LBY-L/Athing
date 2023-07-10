de731cc9f433e4856e879de933aa736de8f490d9
LBY <93894290+LBY-L@users.noreply.github.com>
2023-07-09 23:34:27 -0600

    Update thing.sh

--- thing.sh
+++ thing.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-xfce4-terminal -e code-server
-xfce4-terminal -e playit
-xfce4-terminal -e "micro ~/.config/code-server/config.yaml"
+xfce4-terminal -e code-server & xfce4-terminal -e playit & xfce4-terminal -e "micro ~/.config/code-server/config.yaml"
